8cda2745fb1b2f1bbabbef1d64076a59916cd6c0
emilliedev <emillie.souza@sptech.school>
2022-11-20 21:58:14 -0300

    deletar funcionando - tentando mostrar lugar

--- site/src/models/diarioModel.js
+++ site/src/models/diarioModel.js
@@ -77,7 +77,7 @@ function editar(novaDescricao, idDiario) {
 function deletar(idDiario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idDiario);
     var instrucao = `
-        DELETE FROM Diario WHERE id = ${idDiario};
+        DELETE FROM Diario WHERE idDiario = ${idDiario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
