bb2dacbf2fb3b1edc8d12a2ef4c6b5f4cf2cffa2
emilliedev <emillie.souza@sptech.school>
2022-12-08 21:25:24 -0300

    pastas organizadas

--- site/app.js
+++ site/app.js
@@ -10,8 +10,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
 var diarioRouter = require("./src/routes/diario");
 
 app.use(express.json());
@@ -22,9 +20,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
 app.use("/diario", diarioRouter);
-app.use("/medidas", medidasRouter)
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
